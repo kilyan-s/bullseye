301c7b4fa26d0c7a2c636fd8f714965ffc762ec7
Kilyan SOCKALINGUM <sockalingum.kilyan@gmail.com>
2020-02-12 14:23:48 -0500

    Xcode project setup.

---
+++ bullseye/bullseye/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  bullseye
+//
+//  Created by Kilyan SOCKALINGUM on 12/02/2020.
+//  Copyright © 2020 Kilyan SOCKALINGUM. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
