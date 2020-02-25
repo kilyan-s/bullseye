80aeccf855e9a9118efb0ab03cebb7b3be693907
Kilyan SOCKALINGUM <sockalingum.kilyan@gmail.com>
2020-02-25 12:23:59 -0500

    Add UI components, bind slider value to @State variable, when the hit me button is pressed show an alert that print the slider's rounded value.

--- bullseye/bullseye/ContentView.swift
+++ bullseye/bullseye/ContentView.swift
@@ -11,29 +11,63 @@ import SwiftUI
 struct ContentView: View {
     
     @State var alertIsVisible: Bool = false
+    @State var sliderValue: Double = 50.0
     
     var body: some View {
         VStack {
-            Text("Hello, World!")
-                .fontWeight(.semibold)
-                .foregroundColor(Color.black)
-                .multilineTextAlignment(.center)
+            Spacer()
+            //Target row
+            HStack {
+                Text("Put the bullseye as close as you can to:")
+                Text("100")
+            }
+            
+            Spacer()
+            //Slider row
+            HStack {
+                Text("1")
+                Slider(value: self.$sliderValue, in: 1...100)
+                Text("100")
+            }
+            
+            Spacer()
+            //Button row
             Button(action: {
                 print("Button was pressed")
                 self.alertIsVisible = true
             }) {
-                Text(/*@START_MENU_TOKEN@*/"Hit me"/*@END_MENU_TOKEN@*/)
+                Text("Hit me")
             }
             .alert(isPresented: $alertIsVisible) { () -> Alert in
-                return Alert(title: Text("Hello there"), message: Text("This is my first popup"), dismissButton: .default(Text("Awesome")))
-                
+                let intSliderValue: Int = Int(self.sliderValue.rounded())
+                return Alert(title: Text("Hello there"), message: Text("The slider's value is \(intSliderValue)."), dismissButton: .default(Text("Awesome")))
             }
+            
+            Spacer()
+            //Score row
+            HStack {
+                Button(action: {}) {
+                    Text("Start over")
+                }
+                Spacer()
+                Text("Score:")
+                Text("99999")
+                Spacer()
+                Text("Round:")
+                Text("999")
+                Spacer()
+                Button(action: {}) {
+                    Text("Info")
+                }
+            }
+            .padding(.bottom, 20)
+            
         }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        ContentView().previewLayout(.fixed(width: 896, height: 414))
     }
 }
